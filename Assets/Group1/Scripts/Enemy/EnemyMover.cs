a45d53e2b45c5f6368f996c0427c714e540ae597
Konev_Daniil <konev.danKo@yandex.ru>
2021-09-22 23:23:43 +0300

    Refactoring

---
+++ Assets/Group1/Scripts/Enemy/EnemyMover.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMover : MonoBehaviour
+{
+    [SerializeField] private float _speed = 2f;
+    [SerializeField] private float _radius = 4f;
+
+    private Vector3 _target;
+
+
+    private void Start()
+    {
+        _target = Random.insideUnitCircle * _radius;
+    }
+
+    private void Update()
+    {
+        transform.position = Vector3.MoveTowards(transform.position, _target, _speed * Time.deltaTime);
+        if (transform.position == _target)
+            _target = Random.insideUnitCircle * _radius;
+    }
+}
